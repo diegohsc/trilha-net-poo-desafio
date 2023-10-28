735aac2255442fee276322c1bb3055a91a1d5bbc
diegohsc <diegohsc@gmail.com>
2023-10-28 18:30:56 -0300

    resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("001","1100","0000000001",1024);
+nokia.Numero = "987654321";
+nokia.InstalarAplicativo("Facebook");
+
+Iphone iphone = new Iphone("002","001","0000000002",2048);
+iphone.Numero = "321123456";
+iphone.InstalarAplicativo("WhatsApp");
+
+//onsole.WriteLine(iphone.Numero);
